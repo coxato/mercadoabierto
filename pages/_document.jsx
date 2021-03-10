3a07dd1f5d00d30abfec525c5a52af8e5be43243
Carlos Martinez <edu6jas@gmail.com>
2021-03-10 18:36:01 -0400

    perfect og:image in meta tag :)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,12 +8,22 @@ class MyExtendedDocument extends Document{
                     <link rel="stylesheet" href="/css/autocomplete.css" />
                     <link rel="icon" type="image/png" href="/images/logo-v3.png" />
                     <meta name="description" content="Mercadoabierto " />
-                    <meta property="og:image" content="/images/og-image.png" />
 
                     <meta property="og:title" content="Mercadoabierto" />
                     <meta property="og:site_name" content="Mercadoabierto"/>
                     <meta property="og:description" content="The best marketplace for sell and buy tech." />
+                    <meta property="og:image" content="https://mercadoabierto.vercel.app/images/og.png" />
+
+                    {/* Twitter Meta Tags */}
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:domain" content="mercadoabierto.vercel.app" />
+                    <meta property="twitter:url" content="https://mercadoabierto.vercel.app/" />
+                    <meta name="twitter:title" content="Mercadoabierto" />
+                    <meta name="twitter:description" content="The best marketplace for sell and buy tech." />
+                    <meta name="twitter:image" content="https://mercadoabierto.vercel.app/images/og-image.png" />
                 </Head>
+
+
                 <body className="my-super-custom-css-class">
                     <Main />
                     <NextScript />
